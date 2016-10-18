155a0bfae5a5bb01476e468a7cb60fef172e9bc8
Đorđe Relić <djordje.relic@ymail.com>
2016-10-18 12:19:43 +0200

    Implemented reading query string from command line

--- run-analytics.py
+++ run-analytics.py
@@ -1,8 +1,37 @@
 from twitter_api import *
 from text_analytics_api import *
+import sys
 
-query = "Samsung"
-language = "en"
+def printHelp():
+    print("\nUsage: python run-analytics.py [OPTION] [OPTION] ..")
+    print("[OPTION]:")
+    print("\t--language=<LN>\twhere <LN> takes value of two letter code for certain language (e.g. en, ru etc). Default lang is English (en).")
+    print("\t--query=<QUERY>\twhere <QUERY> takes value of query you want to search on Twitter (e.g. Twitter). This value has to be passed.\n")
+
+query = ""
+language = ""
+
+for arg in sys.argv:
+    if (arg == __file__):
+        continue
+    elif arg.startswith("--query"):
+        query = arg.split('=')[1]
+    elif arg.startswith("--language"):
+        language = arg.split('=')[1]
+    elif arg == ("--help"):
+        printHelp()
+        sys.exit()
+    else:
+        print("Unknown option. Run python run-analytics.py --help for details.")
+        sys.exit()
+
+if (query == ""):
+    print("ERROR: Invalid parameter passed. Query cannot be an empty string.")
+    sys.exit()
+
+if (language == ""):
+    print("WARNING: No language is selected, default lang is set.")
+    language = "en"
 
 print("Fetching tweets ...")
 twApi = TwitterApi()
